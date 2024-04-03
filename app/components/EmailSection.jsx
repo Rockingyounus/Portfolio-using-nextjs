ffe4d588ee2f6fffe7a56e8cc5fb0dd99000798a
Rockingyounus <younusansari1253@gmail.com>
2024-04-04 01:24:04 +0530

    changes in eamilsection

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -11,11 +11,7 @@ const EmailSection = () => {
       <div className="bg-gradient-to-t from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2"></div>
       <div className='z-10'>
         <h5 className='text-xl font-bold text-white my-2'>Let Connect</h5>
-        <p className='text-[#ADB7BE] mb-4 max-w-md'>
-            I am Currently looking for new opportunities my inbox is always open
-            Whether you have a question or just want to say Hi I will try my best 
-            to get back to you
-        </p>
+      
         <div className='socials flex flex-row gap-2'>
             <Link href="https://Github.com">
               <FaGithub/>
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -26,10 +26,7 @@ const HeroSection = () => {
                   repeat={Infinity}
                 />
               </h1>
-              <p className='text-[#ADB7BE] text-lg mb-6 sm:text-lg lg:text-xl'>
-              Hi I am Younus Full Stack web developer i have relavent experience in web developer
-               And i passionate about to learning new skills
-              </p>
+             
               <div>
                 <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button>
                 <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
